650749a3ef0aac8cb8471c5bf2510ad6681dd34b
YeisonGomez <yeigomez97@hotmail.com>
2017-01-06 15:42:16 -0500

    edit launcher icon

--- app/src/main/java/com/example/yeisongomez/gestordetareas/MainActivity.java
+++ app/src/main/java/com/example/yeisongomez/gestordetareas/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -39,6 +40,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //TODO Agregar icono en el header
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setIcon(R.mipmap.ic_launcher);
 
         //TODO Evento boton flotante
         addTask = (FloatingActionButton) findViewById(R.id.fab);
