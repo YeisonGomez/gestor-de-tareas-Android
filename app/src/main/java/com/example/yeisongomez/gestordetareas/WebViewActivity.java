764eb17d16047c67dd85ed39641de84b354f836f
YeisonGomez <yeigomez97@hotmail.com>
2017-01-08 10:38:28 -0500

    config webview

--- app/src/main/java/com/example/yeisongomez/gestordetareas/WebViewActivity.java
+++ app/src/main/java/com/example/yeisongomez/gestordetareas/WebViewActivity.java
@@ -3,6 +3,7 @@ package com.example.yeisongomez.gestordetareas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebViewActivity extends AppCompatActivity {
 
@@ -12,6 +13,15 @@ public class WebViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_web_view);
 
         WebView webview = (WebView) findViewById(R.id.webview_id);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setBuiltInZoomControls(true); //Habilitar Zoom
+
         webview.loadUrl("http://www.google.com.co");
+
+        webview.setWebViewClient(new WebViewClient(){
+             public boolean shouldOverriceUrlLoading(WebView view, String url){
+                return false;
+            }
+        });
     }
 }
